b324eabecd6c62ce61f41641eede773841c74ec1
Vivek Singh <viveksingh9301@gmail.com>
2025-06-14 13:28:10 +0530

    Create http-server using port from .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("Welcome to JARVIS authentication system.")
+	fmt.Println("Welcome to JARVIS authentication system.")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading the .env file.")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server started at port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
