b133cecc3200dd78f831dd1643bc3eb00ee8a63a
Vivek Singh <viveksingh9301@gmail.com>
2025-06-15 10:55:05 +0530

    Implement user registration logic with request payload validation

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,9 +1,25 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/viveksingh-01/jarvis-auth/models"
+)
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// Create a User struct to hold the registration data
+	// This struct should match the expected JSON structure in the request body
+	var user models.User
+
+	// Decode the JSON request body into the User struct and handle any decoding errors
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request payload: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println(user)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
