4552dac1df38a2726845a1dbb5bde3f8d3e503e4
Vivek Singh <viveksingh9301@gmail.com>
2025-06-15 09:41:05 +0530

    Implement authentication routes for login and registration

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,1 +1,11 @@
 package controllers
+
+import "net/http"
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/viveksingh-01/jarvis-auth/controllers"
 )
 
-func RegisterAuthRoutes(r *mux.Router) {
-	// TODO
+func RegisterAuthRoutes(router *mux.Router) {
+	router.HandleFunc("/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/register", controllers.Register).Methods("POST")
 }
